2fbc651641ce957934bfc47bfe12b83997e316e7
Thai Lam <thaivanlam171@gmail.com>
2024-11-23 12:20:54 +0700

    change all case-sensitive to case-insensitive

--- Room/Delete.php
+++ Room/Delete.php
@@ -1,6 +1,6 @@
 <?php
 include_once 'connection.php';
-$sql = "DELETE FROM Room WHERE Room_Number='" . $_GET["Room_Number"] . "'";
+$sql = "DELETE FROM room WHERE Room_Number='" . $_GET["Room_Number"] . "'";
 if (mysqli_query($conn, $sql)) {
     include 'DispRoom.php';
 } else {
